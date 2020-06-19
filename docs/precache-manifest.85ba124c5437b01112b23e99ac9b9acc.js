ab0e91d60e39ec3b525f5d2e7be3c2d643992a88
Juan Pablo Martínez Romero <44387027+jpstorm21@users.noreply.github.com>
2020-06-19 17:37:45 -0400

    app finish

---
+++ docs/precache-manifest.85ba124c5437b01112b23e99ac9b9acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba69bc36ecdfb5ea75bea9eab2d70a8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0399720d9aa8feb69fc0",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0399720d9aa8feb69fc0",
+    "url": "/static/js/main.c2c380ce.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
